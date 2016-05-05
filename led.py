8b6fc169f825bb16017a813144ef2484b8d4ca96
Alex Ashcroft <gnasher98@gmail.com>
2016-05-05 14:43:59 +0000

    5/5/16

--- led.py
+++ led.py
@@ -1,2 +1,11 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+print "LED on"
+GPIO.output(18,GPIO.HIGH)
+time.sleep(3)
+print "LED off"
+GPIO.output(18,GPIO.LOW)
+#6 In on a rasberry pi is pin number 18 
